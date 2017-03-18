dfe23579aaecfa8297b5eddaa1f234a58fcd31e1
NikTeacherJava <nik.teacher.java@gmail.com>
2017-03-18 14:33:56 +0300

    Modified person class

--- src/Person.java
+++ src/Person.java
@@ -1,4 +1,38 @@
 public class Person {
     private String name;
-    private int age;
+    private String surname;
+    private double salary;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public double getSalary() {
+        return salary;
+    }
+
+    public void setSalary(double salary) {
+        this.salary = salary;
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "name='" + name + '\'' +
+                ", surname='" + surname + '\'' +
+                ", salary=" + salary +
+                '}';
+    }
 }
